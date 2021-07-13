8cf4fde4f11ebb3b1a16cef01cbf46b6906b4753
Tom Whitwell <tom@whi.tw>
2021-07-13 14:52:16 +0100

    add some kind of crappy readme to the 'homepage'

--- ical-transparentifier/server.py
+++ ical-transparentifier/server.py
@@ -11,7 +11,17 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "title":        "iCal Transparentifier",
+        "description":  ["If you (like me) have the pagerduty ics imported into your google calendar, I've made a little quality of life proxy thing that can sit in between google and pagerduty that:",
+                        ["* Updates all the events to be marked as 'free' not busy, so it doesn't cause all your events to get all squashed up when the calendar is shown, and just sits in the background",
+                        "* allows you to strip events with a certain name out"]],
+        "github":       "https://github.com/whi-tw/ical-transparentifier",
+        "usage":        ["https://ical-transparentifier.herokuapp.com/cal/${CALENDAR_URL}?strip=in+hours",
+                        ["This example will strip any event with 'in hours' in the name out of the calendar"],
+                        "The full url would look like:",
+                        ["https://ical-transparentifier.herokuapp.com/cal/webcal://some-org.pagerduty.com/private/nz5gdccah69xpps5dsvzrpcgi4bvi5beyufxagusgpjhea1srmolobqb2gf6byxd/feed?strip=in+hours"]]
+    }
 
 
 @app.get("/cal/{cal_uri:path}", status_code=status.HTTP_400_BAD_REQUEST)
